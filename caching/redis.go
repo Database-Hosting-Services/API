6cfdabfda996a468f86f48fbf255f0ce10ade854
GergesHany <gergeshany179@gmail.com>
2025-03-01 20:52:26 +0200

    Redis Structure

---
+++ caching/redis.go
@@ -0,0 +1,61 @@
+package caching
+
+import (
+	"context"
+	"time"
+	"github.com/go-redis/redis/v8"
+)
+
+// RedisClient wraps the go-redis client.
+type RedisClient struct {
+    Client *redis.Client
+}
+
+// NewRedisClient initializes and returns a new RedisClient instance.
+func NewRedisClient(addr, password string, db int) *RedisClient {
+    rdb := redis.NewClient(&redis.Options{
+        Addr:     addr,
+        Password: password,
+        DB:       db,
+    })
+
+    ctx := context.Background()
+    if err := rdb.Ping(ctx).Err(); err != nil {
+        panic(err)
+    }
+
+    return &RedisClient{Client: rdb}
+}
+
+// Set sets a key-value pair in Redis with the given expiration.
+func (r *RedisClient) Set(key string, value interface{}, expiration time.Duration) error {
+    ctx := context.Background()
+    return r.Client.Set(ctx, key, value, expiration).Err()
+}
+
+// Get retrieves the value associated with the given key.
+func (r *RedisClient) Get(key string) (string, error) {
+    ctx := context.Background()
+    return r.Client.Get(ctx, key).Result()
+}
+
+// Delete deletes the value associated with the given key
+func (r *RedisClient) Delete(key string) error {
+    ctx := context.Background()
+    return r.Client.Del(ctx, key).Err()
+}
+
+// Exists checks if a key exists in Redis.
+func (r *RedisClient) Exists(key string) (bool, error) {
+    ctx := context.Background()
+    exists, err := r.Client.Exists(ctx, key).Result()
+    if err != nil {
+        return false, err
+    }
+    return exists > 0, nil
+}
+
+// Close closes the Redis client connection.
+func (r *RedisClient) Close() error {
+	return r.Client.Close()
+}
--- go.mod
+++ go.mod
@@ -11,7 +11,10 @@ require (
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
