05ff07e0f4bc358ce241ea6aa0b55f29a203a2bb
rag-nar1 <ragnar1242003@gmail.com>
2025-03-01 07:01:28 +0200

    error and success response calls CreateResponse insted of (ErrorResponse, SuccessResponse

--- response/errors.go
+++ response/errors.go
@@ -2,26 +2,18 @@ package response
 
 import "net/http"
 
-func ErrorResponse(w http.ResponseWriter, status int, message string, err error) {
-	response := Response{
-		Status:  status,
-		Message: message,
-		Error:   err.Error(),
-	}
-	sendResponse(w, status, response)
-}
 
 // you can use one of these frequently used response for more code readability
 //
 
 func BadRequest(w http.ResponseWriter, message string, err error) {
-	ErrorResponse(w, http.StatusBadRequest, message, err)
+	CreateResponse(w, http.StatusBadRequest, message, err, nil)
 }
 
 func NotFound(w http.ResponseWriter, message string, err error) {
-	ErrorResponse(w, http.StatusNotFound, message, err)
+	CreateResponse(w, http.StatusNotFound, message, err, nil)
 }
 
 func InternalServerError(w http.ResponseWriter, message string, err error) {
-	ErrorResponse(w, http.StatusInternalServerError, message, err)
+	CreateResponse(w, http.StatusInternalServerError, message, err, nil)
 }
