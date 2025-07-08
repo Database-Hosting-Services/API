fa802de962f76f5c39301005af6b3ba3a98b6499
rag-nar1 <ragnar1242003@gmail.com>
2025-07-08 15:53:24 +0300

    fix: include request URI in response logging for better traceability

--- response/response.go
+++ response/response.go
@@ -41,6 +41,7 @@ func CreateResponse(w http.ResponseWriter, r *http.Request, status int, message 
 		"user-oid":  r.Context().Value("user-oid"),
 		"user-name": r.Context().Value("user-name"),
 		"status-code": status,
+		"URI": r.RequestURI,
 	}
 	if err != nil {
 		response = &Response{
