ea6edffadcf4507aa739694eeaf1b98541ab6f16
rag-nar1 <ragnar1242003@gmail.com>
2025-07-08 20:49:21 +0300

    fix: refactor project routes to separate ownership check middleware for clarity

--- projects/routes.go
+++ projects/routes.go
@@ -8,14 +8,16 @@ import (
 
 func DefineURLs() {
 	router := config.Router.PathPrefix("/api/projects").Subrouter()
-	router.Use(middleware.JwtAuthMiddleware, middleware.CheckOwnership)
-
+	router.Use(middleware.JwtAuthMiddleware)
+	
 	router.Handle("", middleware.Route(map[string]http.HandlerFunc{
 		http.MethodPost: CreateProject(config.App),
 		http.MethodGet:  GetProjects(config.App),
 	}))
-
-	router.Handle("/{project_id}", middleware.Route(map[string]http.HandlerFunc{
+	
+	singleTablerouter := config.Router.PathPrefix("/api/projects/{project_id}").Subrouter()
+	singleTablerouter.Use(middleware.JwtAuthMiddleware, middleware.CheckOwnership)
+	singleTablerouter.Handle("", middleware.Route(map[string]http.HandlerFunc{
 		http.MethodGet:    getSpecificProject(config.App),
 		http.MethodPatch:  updateProject(config.App),
 		http.MethodDelete: DeleteProject(config.App),
