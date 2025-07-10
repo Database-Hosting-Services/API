eae4fc91c99bfcec1c357ef0d0fa230d2c3af6ff
rag-nar1 <ragnar1242003@gmail.com>
2025-07-10 22:10:28 +0300

    fix(config): update deploy flag to true for production environment

--- config/application.go
+++ config/application.go
@@ -75,7 +75,7 @@ func loadEnv() {
 }
 
 const (
-	deploy = false // Set to true if running in production, false for development
+	deploy = true // Set to true if running in production, false for development
 )
 
 func Init(infoLog, errorLog *log.Logger) {
