9c585bf2cddb94538d0aae977540d2e520caea8b
GergesHany <gergeshany179@gmail.com>
2025-07-11 05:29:35 +0300

    refactor(schemas): remove unused getDatabaseByName function

--- schemas/repository.go
+++ schemas/repository.go
@@ -1,7 +1,6 @@
 package schemas
 
 import (
-	"DBHS/projects"
 	"DBHS/utils"
 	"context"
 	"database/sql"
@@ -9,15 +8,6 @@ import (
 
 	"github.com/georgysavva/scany/v2/pgxscan"
 )
-
-func getDatabaseByName(ctx context.Context, DB utils.Querier, name string) (projects.DatabaseConfig, error) {
-	var database projects.DatabaseConfig
-	err := pgxscan.Get(ctx, DB, &database, GetDatabaseByName, name)
-	if err != nil {
-		return projects.DatabaseConfig{}, err
-	}
-	return database, nil
-}
 
 func getDatabaseTableName(ctx context.Context, DB utils.Querier, tableOID string) (string, error) {
 	var tableName string
