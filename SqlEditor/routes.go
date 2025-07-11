f689377ff45eb6c922becdf5a28d16db6c8df70e
GergesHany <gergeshany179@gmail.com>
2025-07-11 05:23:14 +0300

    Update The /api/projects/{project_id}/sqlEditor/run-query endpoint method

--- SqlEditor/routes.go
+++ SqlEditor/routes.go
@@ -11,6 +11,6 @@ func DefineURLs() {
 	router.Use(middleware.JwtAuthMiddleware)
 
 	router.Handle("/run-query", middleware.Route(map[string]http.HandlerFunc{
-		http.MethodGet: RunSqlQuery(config.App),
+		http.MethodPost: RunSqlQuery(config.App),
 	}))
 }
