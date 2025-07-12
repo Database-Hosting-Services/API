ede8bb5064f2088ba6c166846e0eada0d639198c
rag-nar1 <ragnar1242003@gmail.com>
2025-07-13 02:39:13 +0300

    Update SQL queries in SqlQueries.go to use COALESCE for better handling of null values in execution time and query counts.

--- analytics/SqlQueries.go
+++ analytics/SqlQueries.go
@@ -11,8 +11,8 @@ const (
 
 	GET_TOTAL_TimeAndQueries = `
 		SELECT
-			ROUND(SUM(total_exec_time)::numeric, 2) as total_time_ms,
-			SUM(calls) as total_queries
+			COALESCE(ROUND(SUM(total_exec_time)::numeric, 2), 0) as total_time_ms,
+			COALESCE(SUM(calls), 0) as total_queries
 		FROM pg_stat_statements pss
 		JOIN pg_database pd ON pss.dbid = pd.oid
 		WHERE pd.datname = $1
@@ -20,9 +20,9 @@ const (
 
 	GET_READ_WRITE_CPU = `
 		SELECT                                   
-			SUM(CASE WHEN query ILIKE 'SELECT%' THEN calls ELSE 0 END) as read_queries,
-			SUM(CASE WHEN query ILIKE ANY(ARRAY['INSERT%', 'UPDATE%', 'DELETE%']) THEN calls ELSE 0 END) as write_queries,
-			ROUND(SUM(total_exec_time)::numeric, 2) as total_cpu_time_ms
+			COALESCE(SUM(CASE WHEN query ILIKE 'SELECT%' THEN calls ELSE 0 END), 0) as read_queries,
+			COALESCE(SUM(CASE WHEN query ILIKE ANY(ARRAY['INSERT%', 'UPDATE%', 'DELETE%']) THEN calls ELSE 0 END), 0) as write_queries,
+			COALESCE(ROUND(SUM(total_exec_time)::numeric, 2), 0) as total_cpu_time_ms
 		FROM pg_stat_statements pss
 		JOIN pg_database pd ON pss.dbid = pd.oid
 		WHERE pd.datname = current_database()
@@ -31,13 +31,13 @@ const (
 
 	GET_ALL_CURRENT_STORAGE = `SELECT created_at::text, data->>'Management storage', data->>'Actual data' FROM analytics WHERE type = 'Storage' and "projectId" = $1 ORDER BY created_at DESC;`
 
-	GET_ALL_EXECUTION_TIME_STATS = `SELECT created_at::text, (data->>'total_time_ms')::numeric, (data->>'total_queries')::bigint FROM analytics WHERE type = 'ExecutionTimeStats' AND "projectId" = $1;	`
+	GET_ALL_EXECUTION_TIME_STATS = `SELECT created_at::text, COALESCE((data->>'total_time_ms')::numeric, 0), COALESCE((data->>'total_queries')::bigint, 0) FROM analytics WHERE type = 'ExecutionTimeStats' AND "projectId" = $1;	`
 
-	GET_ALL_DATABASE_USAGE_STATS = `SELECT created_at::text, (data->>'read_write_cost')::numeric, (data->>'cpu_cost')::numeric, (data->>'total_cost')::numeric FROM analytics WHERE type = 'DatabaseUsageStats' AND "projectId" = $1;`
+	GET_ALL_DATABASE_USAGE_STATS = `SELECT created_at::text, COALESCE((data->>'read_write_cost')::numeric, 0), COALESCE((data->>'cpu_cost')::numeric, 0), COALESCE((data->>'total_cost')::numeric, 0) FROM analytics WHERE type = 'DatabaseUsageStats' AND "projectId" = $1;`
 
 	// Queries to get the last records for each type of analytics
 
-	GET_LAST_EXECUTION_TIME_STATS = `SELECT created_at::text, (data->>'total_time_ms')::numeric, (data->>'total_queries')::bigint FROM analytics WHERE type = 'ExecutionTimeStats' AND "projectId" = $1 ORDER BY created_at DESC LIMIT 1;`
+	GET_LAST_EXECUTION_TIME_STATS = `SELECT created_at::text, COALESCE((data->>'total_time_ms')::numeric, 0), COALESCE((data->>'total_queries')::bigint, 0) FROM analytics WHERE type = 'ExecutionTimeStats' AND "projectId" = $1 ORDER BY created_at DESC LIMIT 1;`
 
-	GET_LAST_DATABASE_USAGE_STATS = `SELECT created_at::text, (data->>'read_write_cost')::numeric, (data->>'cpu_cost')::numeric, (data->>'total_cost')::numeric FROM analytics WHERE type = 'DatabaseUsageStats' AND "projectId" = $1 ORDER BY created_at DESC LIMIT 1;`
+	GET_LAST_DATABASE_USAGE_STATS = `SELECT created_at::text, COALESCE((data->>'read_write_cost')::numeric, 0), COALESCE((data->>'cpu_cost')::numeric, 0), COALESCE((data->>'total_cost')::numeric, 0) FROM analytics WHERE type = 'DatabaseUsageStats' AND "projectId" = $1 ORDER BY created_at DESC LIMIT 1;`
 )
