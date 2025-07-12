1f09947802fb00221b47120db9bcff1c1c020a76
rag-nar1 <ragnar1242003@gmail.com>
2025-07-12 16:04:33 +0300

    negatice cost ha

--- analytics/service.go
+++ analytics/service.go
@@ -194,8 +194,15 @@ func GetALLDatabaseUsageStats(ctx context.Context, db *pgxpool.Pool, projectOid 
 		if err := rows.Scan(&record.Timestamp, &record.ReadWriteCost, &record.CPUCost, &record.TotalCost); err != nil {
 			return nil, *api.NewApiError("Internal server error", 500, errors.New("failed to scan database usage record: "+err.Error()))
 		}
-		record.CPUCost *= 1000000
-		record.ReadWriteCost *= 1000000
+		record.CPUCost *= 10000000
+		record.ReadWriteCost *= 10000000
+		if record.CPUCost < 0 {
+			record.CPUCost *= -1
+		}
+
+		if record.ReadWriteCost < 0 {
+			record.ReadWriteCost *= -1
+		}
 		record.TotalCost = record.CPUCost + record.ReadWriteCost
 		usageRecords = append(usageRecords, record)
 	}
