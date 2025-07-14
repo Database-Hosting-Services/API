18e93d96a36d9ee81a7309c060cfaadba6e32cb5
GergesHany <gergeshany179@gmail.com>
2025-07-14 09:02:59 +0300

    Update the CalculateCosts formula

--- analytics/utils.go
+++ analytics/utils.go
@@ -12,8 +12,8 @@ import (
 // CalculateCosts calculates the costs associated with database usage based on read/write queries and CPU time.
 func (d *DatabaseUsageStats) CalculateCosts() DatabaseUsageCost {
 	Cost := DatabaseUsageCost{
-		ReadWriteCost: float64(d.ReadQueries)/1_000_000*1.00 + float64(d.WriteQueries)/1_000_000*1.50,
-		CPUCost:       (d.TotalCPUTimeMs / 1000 / 3600) * 0.000463,
+		ReadWriteCost: float64(d.ReadQueries)*1.00 + float64(d.WriteQueries)*1.50,
+		CPUCost:       d.TotalCPUTimeMs * 1.50, 
 	}
 	Cost.TotalCost = Cost.ReadWriteCost + Cost.CPUCost
 	return Cost
